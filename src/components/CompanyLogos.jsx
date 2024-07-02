f65449b6c7f30b295a6c694afb0a8bbbbe1300a3
annisa-nye <annisanye99@gmail.com>
2024-07-02 15:34:09 +1000

    first commit

--- src/components/CompanyLogos.jsx
+++ src/components/CompanyLogos.jsx
@@ -1,10 +1,11 @@
 import { companyLogos } from "../constants";
+import { brainwave } from "../assets";
 
 const CompanyLogos = ({ className }) => {
   return (
     <div className={className}>
       <h5 className="tagline mb-6 text-center text-n-1/50">
-        Helping people create beautiful content at
+        Brainwave
       </h5>
       <ul className="flex">
         {companyLogos.map((logo, index) => (
@@ -12,7 +13,7 @@ const CompanyLogos = ({ className }) => {
             className="flex items-center justify-center flex-1 h-[8.5rem]"
             key={index}
           >
-            <img src={logo} width={134} height={28} alt={logo} />
+            <img src={brainwave} width={134} height={28} alt={logo} />
           </li>
         ))}
       </ul>
